918e3b7001ccf560d1bfdebf05d3bfc4b0ffec2a
Zhenya <kompovich@gmail.com>
2016-04-14 11:22:48 +0300

    Update InvocationPolicy.java

--- src/policy/InvocationPolicy.java
+++ src/policy/InvocationPolicy.java
@@ -6,5 +6,5 @@ import java.lang.reflect.Method;
  * Created by Nicki on 12.04.2016.
  */
 public interface InvocationPolicy {
-    <T> T delegateCall(Method method, Object caller, Object... args);
+    Object delegateCall(Method method, Object caller, Object... args);
 }
